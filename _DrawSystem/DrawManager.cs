b83a6d4409aef35fbc60166a7392d741fc0be2f8
an-alch3mist <kaeya000@gmail.com>
2025-11-06 01:54:31 +0600

    v0.7.7 v0.2

--- _DrawSystem/DrawManager.cs
+++ _DrawSystem/DrawManager.cs
@@ -7,17 +7,17 @@ using SPACE_UTIL;
 
 namespace SPACE_DrawSystem
 {
-	
 	public static class DRAW
 	{
-		public static Transform DrawHolder;
+		public static Transform DrawHolder; // make sure it exist and created at runtime
 	}
 
 	/// <summary>
-	/// Create a new persistent line
-	/// Line.create(id: "somthng").setA(a).setB(b).setCol(color);
-	/// Line line = new Line(); line.init(name: "somthng").setA(a).setB(b).setCol(color);
+	/// Line.create("myLine").setA(a).setB(b); // ← Cleaner for persistent lines
 	/// </summary>
+	// Line line = new Line().init("myLine").setA(a).setB(b);
+
+	// built to work when compile and re-run is made without the use of Awake() from any method 
 	public class Line
 	{
 		// ============================================================
@@ -105,6 +105,12 @@ namespace SPACE_DrawSystem
 			this.UpdatePositions();
 			return this;
 		}
+		public Line setN(Vector3 val)
+		{
+			this.bRef = this.a + val;
+			this.UpdatePositions();
+			return this;
+		}
 		public Line setCol(Color val)
 		{
 			lr.startColor = val;
@@ -118,9 +124,25 @@ namespace SPACE_DrawSystem
 			return this;
 		}
 
-		// when called as: this.line.init(name: "hoverLine") .setA(ray.origin).setB(ray.origin + ray.direction * rayDist); // still creating multiple gameObject Instance
-		public Line init(string name = "line", Color? color = null, float e = 1f / 50)
+		public string id { get; private set; }
+
+		// when called as Line.create(id) .setA(ray.origin) .setN(ray.SetN);
+		public static Dictionary<string, Line> MAP_IdLine;
+
+		/// <summary>
+		/// try to get Line of given id, if doesn;t exist yet? create new one and return.
+		/// </summary>
+		/// <param name="id">unique line id</param>
+		/// <param name="color">line color, could be set later via .setCol(Color)</param>
+		/// <param name="e">thickness of line, could be set later via .setE(float)</param>
+		/// <returns></returns>
+		public static Line create(object id, Color? color = null, float e = 1f / 50)
 		{
+			if (Line.MAP_IdLine == null)
+			{
+				Debug.Log(C.method(null, "lime", "init MAP_IdLine<>"));
+				Line.MAP_IdLine = new Dictionary<string, Line>();
+			}
 			if (DRAW.DrawHolder == null) // occurs first line
 			{
 				if (GameObject.Find("DrawHolder") != null)
@@ -128,10 +150,28 @@ namespace SPACE_DrawSystem
 				DRAW.DrawHolder = new GameObject("DrawHolder").transform;
 				Debug.Log("initialized DRAWHolder now".colorTag("lime"));
 			}
+
+			string idStr = id.ToString();
+			if(Line.MAP_IdLine.ContainsKey(idStr) == true)
+				return Line.MAP_IdLine[idStr];
+			else
+			{
+				Debug.Log(C.method(null, "lime", $"created a new line and linked to MAP_IdLine"));
+				Line line = new Line().init(name: idStr, color, e);  // DRAW.DrawHolder is handled inside init()
+				Line.MAP_IdLine[idStr] = line;
+				return line;
+			}
+		}
+
+		// (private for now) when called as: this.line.init(name: "hoverLine") .setA(ray.origin).setB(ray.origin + ray.direction * rayDist); // still creating multiple gameObject Instance
+		// private constructor, to make Line.create(id) as standard
+		private Line init(string name = "line", Color? color = null, float e = 1f / 50)
+		{
+			this.id = name;
 			if (this.lineObj == null)  // occurs first call of a certain line
 			{
 				this.aRef = Vector3.right * (float)1e6;
-				this.bRef = Vector3.right * (float)1e6 - Vector3.right * 0.01f;
+				this.bRef = Vector3.right * (float)1e6 - Vector3.right * (float)1e-3;
 
 				this.lineObj = new GameObject($"{name}");
 				this.lineObj.transform.SetParent(DRAW.DrawHolder);
@@ -145,11 +185,14 @@ namespace SPACE_DrawSystem
 
 		// << ========================= CHAIN SYNTAX =============================== //
 		// clear line vertex positionsm to somewhere far
-		public Line clear()
+		public Line toggle(bool value = false)
 		{
+			this.lr.enabled = value;
+			/*
 			this.aRef = Vector3.right * (float)1e6;
 			this.bRef = Vector3.right * (float)1e6 - Vector3.right * 0.01f;
 			this.UpdatePositions();
+			*/		
 			return this;
 		}
 		/// <summary>
@@ -157,10 +200,15 @@ namespace SPACE_DrawSystem
 		/// </summary>
 		public void destroy()
 		{
-			if (lineObj != null)
-				UnityEngine.Object.Destroy(lineObj);
+			UnityEngine.Object.Destroy(lineObj);
+			if(Line.MAP_IdLine.ContainsKey(this.id) == true)
+			{
+				Line.MAP_IdLine.Remove(this.id);
+				Debug.Log(C.method(null, "lime", $"success destroying line of id: {this.id} from MAP_IdLine"));
+			}
+			else
+				Debug.Log(C.method(null, "red", $"error destroying line of id: {this.id} in MAP_IdLine , doesn;t exist"));
 		}
-
 
 		// ============================================================
 		// PRIVATE METHODS
